938423f592599e7bb2f5cc24d1ed845b07e097ca
Polina Tochilna <narkada@mail.ru>
2020-10-28 15:09:27 +0300

    map works

--- app/src/main/java/com/example/poc/MapActivity.java
+++ app/src/main/java/com/example/poc/MapActivity.java
@@ -1,14 +1,39 @@
 package com.example.poc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng moscow = new LatLng(55.751688, 37.616791);
+        map.addMarker(new MarkerOptions().position(moscow).title("Wohooooooo"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(moscow));
     }
 }
